0b1fbdbdf7a6a4101b9ec1c7300da022d694e663
khanhngguyen <ntkimkhanh97@gmail.com>
2023-06-29 23:39:54 +0300

    initial project set up

---
+++ MediaPlayer.Tests/src/Domain.Tests/MediaFileTests.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MediaPlayer.Tests.src.Domain.Tests
+{
+    public class MediaFileTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
